5f69dc05630e85f40f597b727d1b54dc81d4a46a
Tobias Bengfort <tobias.bengfort@gmx.net>
2016-11-18 16:53:08 +0100

    wrap in umd

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -1,41 +1,61 @@
-var animate = function(apply, duration) {
-  var start = null;
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory());
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    window.smoothScroll = factory();
+    // if not used as module, execute directly
+    window.smoothScroll.init();
+  }
+})(function() {
+  var animate = function(apply, duration) {
+    var start = null;
 
-  var step = function(timestamp) {
-    if (!start) start = timestamp;
-    var progress = Math.min(1, (timestamp - start) / duration);
-    apply(progress);
-    if (progress < 1) {
-      window.requestAnimationFrame(step);
-    }
+    var step = function(timestamp) {
+      if (!start) start = timestamp;
+      var progress = Math.min(1, (timestamp - start) / duration);
+      apply(progress);
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+
+    window.requestAnimationFrame(step);
   };
 
-  window.requestAnimationFrame(step);
-};
+  var smoothScrollTo = function(endY, container, duration) {
+    container = container || window;
+    duration = duration || 250;
 
-var smoothScrollTo = function(endY, container, duration) {
-  container = container || window;
-  duration = duration || 250;
+    var startY = container.scrollY;
 
-  var startY = container.scrollY;
+    animate(function(progress) {
+      // var f = Math.sin(Math.PI * (progress - 0.5)) / 2 + 0.5;
+      var f = (3 - 2 * progress) * progress * progress;
+      container.scrollTo(0, startY * (1 - f) + endY * f);
+    }, duration);
+  };
 
-  animate(function(progress) {
-    // var f = Math.sin(Math.PI * (progress - 0.5)) / 2 + 0.5;
-    var f = (3 - 2 * progress) * progress * progress;
-    container.scrollTo(0, startY * (1 - f) + endY * f);
-  }, duration);
-};
+  var init = function() {
+    var smoothScrollClick = function(event) {
+      event.preventDefault();
 
-var smoothScrollClick = function(event) {
-  event.preventDefault();
+      var selector = event.currentTarget.getAttribute('href');
+      var scrollY = document.querySelector(selector).offsetTop;
 
-  var selector = event.currentTarget.getAttribute('href');
-  var scrollY = document.querySelector(selector).offsetTop;
+      history.pushState(null, null, selector);
+      smoothScrollTo(scrollY);
+    };
 
-  history.pushState(null, null, selector);
-  smoothScrollTo(scrollY);
-};
+    document.querySelectorAll('[href^="#"]').forEach(function(el) {
+      el.addEventListener('click', smoothScrollClick);
+    });
+  };
 
-document.querySelectorAll('[href^="#"]').forEach(function(el) {
-  el.addEventListener('click', smoothScrollClick);
+  return {
+    animate: animate,
+    smoothScrollTo: smoothScrollTo,
+    init: init,
+  };
 });
